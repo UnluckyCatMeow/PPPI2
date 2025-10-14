a6845410be07ec9e18c70d87fb0cfe457e8a7fb3
UnluckyCatMeow <st4646048@stud.duikt.edu.ua>
2025-10-14 18:55:09 +0300

    test_books.py

--- test_books.py
+++ test_books.py
@@ -1,24 +1,24 @@
 import builtins
+import importlib
+import sys
 
 def test_books_program(monkeypatch, capsys):
-    # Підставимо відповіді, які програма отримає через input()
     inputs = iter([
-        "3",             # Кількість книг
-        "Книга1", "Автор1", "100",  # 1-а книга
-        "Книга2", "Автор2", "200",  # 2-а книга
-        "Книга3", "Автор3", "300",  # 3-я книга
+        "3",
+        "Книга1", "Автор1", "100",
+        "Книга2", "Автор2", "200",
+        "Книга3", "Автор3", "300",
     ])
 
-    # Підміняємо input() щоб він повертав наші значення
     monkeypatch.setattr(builtins, "input", lambda _: next(inputs))
 
-    # Імпортуємо твою програму (вона автоматично виконається)
-    import books
+    if "books" in sys.modules:
+        importlib.reload(sys.modules["books"])
+    else:
+        import books
 
-    # Збираємо увесь вивід програми
     output = capsys.readouterr().out
 
-    # Перевіряємо, що є правильний текст
     assert "Середня кількість сторінок" in output
     assert "Книги з меншою кількістю сторінок" in output
     assert "Книга1" in output
